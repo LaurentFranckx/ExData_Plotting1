35406a28bc9a70d1bba185a89eb42527b4d4dc99
LaurentFranckx <franckx.laurent@gmail.com>
2014-06-04 22:21:48 +0200

    Second version of plot1 R and png

--- plot1.R
+++ plot1.R
@@ -1,19 +1,13 @@
 unzip("exdata-data-household_power_consumption.zip")                            
 consump_raw <- read.csv2("household_power_consumption.txt", nrows = 2075259)
-summary(consump_raw)
-classes <- sapply(consump_raw,class)
-
 consump_raw$Date <- as.Date(as.character(consump_raw$Date), format = "%e/%m/%Y")
-#consump_raw$Time <- as.Date(as.character(consump_raw$Time), format = "%H:%M:%S")
-consump_anal <- consump_raw[consump_raw$Date  == "2007-02-01"  | consump_raw$Date == "2007-02-02"   , ]
-summary(consump_anal)
 
+consump_anal <- consump_raw[consump_raw$Date  == "2007-02-01"  | consump_raw$Date == "2007-02-02"   , ]
 consump_anal$Global_active_power <- as.character(consump_anal$Global_active_power)
 consump_anal <- consump_anal[ consump_anal$Global_active_power != "?", ]
-
 consump_anal$Global_active_power <- as.numeric(consump_anal$Global_active_power)
 
-summary(consump_anal$Global_active_power)
+
 hist(consump_anal$Global_active_power, col = "red", main = "Global active power"
      , xlab = "Global Active Power (kilowatts)")
 dev.copy(png, "plot1.png" , width = 480, height = 480)
