2a4d67977aad19f687630fc9c79c309908401001
LaurentFranckx <franckx.laurent@gmail.com>
2014-06-05 21:32:13 +0200

    Second commit of plot4 R code

--- plot4.R
+++ plot4.R
@@ -11,33 +11,48 @@ consump_anal <- consump_raw[consump_raw$Date2  == "2007-02-01"  | consump_raw$Da
 var_names <- c("Global_active_power", "Global_reactive_power" ,"Voltage","Global_intensity",
                "Sub_metering_1", "Sub_metering_2")
 
-for(measure in var_names){
-        consump_anal[  , measure]   <- as.character(consump_anal[  , measure] )
-        consump_anal <- consump_anal[consump_anal[  , measure] != "?", ]
-        consump_anal[  , measure]   <- as.numeric(consump_anal[  , measure] )
-}
-
-
-
 
 Sys.setlocale("LC_TIME", "English")
 
 png("plot4.png" , width = 480, height = 480)
 par(mfrow = c(2,2))
 
-plot(consump_anal$Time2, consump_anal$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+consump_gap <- consump_anal
+consump_gap$Global_active_power <- as.character(consump_gap$Global_active_power)
+consump_gap <- consump_gap[ consump_gap$Global_active_power != "?", ]
+consump_gap$Global_active_power <- as.numeric(consump_gap$Global_active_power)
+plot(consump_gap$Time2, consump_gap$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
 
-plot(consump_anal$Time2, consump_anal$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+consump_volt <- consump_anal
+consump_volt$Voltage <- as.character(consump_volt$Voltage)
+consump_volt <- consump_volt[ consump_volt$Voltage != "?", ]
+consump_volt$Voltage <- as.numeric(consump_volt$Voltage)
+plot(consump_volt$Time2, consump_volt$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
 
 
-plot(consump_anal$Time2, consump_anal$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
-lines(consump_anal$Time2, consump_anal$Sub_metering_2, col = "red")
-lines(consump_anal$Time2, consump_anal$Sub_metering_3, col = "blue")
+consump_submet <- consump_anal
+consump_submet$Sub_metering_1 <- as.character(consump_submet$Sub_metering_1)
+consump_submet$Sub_metering_2 <- as.character(consump_submet$Sub_metering_2)
+consump_submet$Sub_metering_3 <- as.character(consump_submet$Sub_metering_3)
+consump_submet <- consump_submet[consump_submet$Sub_metering_1 != "?", ]
+consump_submet <- consump_submet[consump_submet$Sub_metering_2 != "?", ]
+consump_submet <- consump_submet[consump_submet$Sub_metering_3 != "?", ]
+consump_submet$Sub_metering_1 <- as.numeric(consump_submet$Sub_metering_1)
+consump_submet$Sub_metering_2 <- as.numeric(consump_submet$Sub_metering_2)
+consump_submet$Sub_metering_3 <- as.numeric(consump_submet$Sub_metering_3)
+plot(consump_submet$Time2, consump_submet$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(consump_submet$Time2, consump_submet$Sub_metering_2, col = "red")
+lines(consump_submet$Time2, consump_submet$Sub_metering_3, col = "blue")
 legend("topright",  lty =1 , col = c("black","red","blue")
        , legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty = "n")
 
 
-plot(consump_anal$Time2, consump_anal$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+
+consump_react <- consump_anal
+consump_react$Global_active_power <- as.character(consump_react$Global_active_power)
+consump_react <- consump_react[ consump_react$Global_active_power != "?", ]
+consump_react$Global_active_power <- as.numeric(consump_react$Global_active_power)
+plot(consump_react$Time2, consump_react$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 
 
 dev.off()
