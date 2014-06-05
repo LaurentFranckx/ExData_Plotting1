4bc5599d9809310fd8009b4b352bd9fd19ffbc32
LaurentFranckx <franckx.laurent@gmail.com>
2014-06-05 20:40:51 +0200

    Second commit of plot3

--- plot3.R
+++ plot3.R
@@ -20,11 +20,14 @@ consump_anal$Sub_metering_3 <- as.numeric(consump_anal$Sub_metering_3)
 
 
 Sys.setlocale("LC_TIME", "English")
+
+png("plot3.png" , width = 480, height = 480)
 plot(consump_anal$Time2, consump_anal$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(consump_anal$Time2, consump_anal$Sub_metering_2, col = "red")
 lines(consump_anal$Time2, consump_anal$Sub_metering_3, col = "blue")
+legend("topright",  lty =1 , col = c("black","red","blue")
+       , legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-dev.copy(png, "plot3.png" , width = 480, height = 480)
 dev.off()
 
     
